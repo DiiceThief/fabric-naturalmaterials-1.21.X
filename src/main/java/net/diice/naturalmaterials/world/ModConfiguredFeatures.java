58d8d2a45d2991755375b673ad2f7e547888c69a
DiiceThief <solarhog87@gmail.com>
2025-06-29 20:32:27 -0600

    first tree complete

--- src/main/java/net/diice/naturalmaterials/world/ModConfiguredFeatures.java
+++ src/main/java/net/diice/naturalmaterials/world/ModConfiguredFeatures.java
@@ -51,7 +51,8 @@ public class ModConfiguredFeatures {
                 new StraightTrunkPlacer(2, 2 ,2) {},
                 BlockStateProvider.of(ModBlocks.CYPRESS_LEAVES),
                 new SpruceFoliagePlacer(UniformIntProvider.create(1, 2), UniformIntProvider.create(1, 2), UniformIntProvider.create(1, 2)),
-                new TwoLayersFeatureSize(1, 0, 1)).dirtProvider(BlockStateProvider.of(Blocks.DIRT)).build());
+                new TwoLayersFeatureSize(1, 0, 1)).dirtProvider(BlockStateProvider.of(Blocks.DIRT))
+                .dirtProvider(BlockStateProvider.of(Blocks.DIRT)).build());
     }
 
 
